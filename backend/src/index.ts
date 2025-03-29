c7af3967b41d33d46f5a253e7320bf1ca260bce7
Kartikey <kadm5224@gmail.com>
2025-03-30 02:00:49 +0530

    Solved Inactive WebSocket Connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,11 @@ const gameManager = new GameManager();
 wss.on('connection', function connection(ws) {
     ws.on('error', console.error);
 
+    const interval = setInterval(() => {
+        ws.ping();
+    }, 30000); // Every 30 seconds
+
     gameManager.addUser(ws);
 
-    ws.on('disconnect', ()=> gameManager.removeUser(ws));
+    ws.on('disconnect', () => gameManager.removeUser(ws));
 });
--- frontend/src/hooks/useSocket.ts
+++ frontend/src/hooks/useSocket.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
-const WS_URL = "ws://localhost:8080";
+// const WS_URL = "ws://localhost:8080";
+const WS_URL = import.meta.env.VITE_WS_URL;
 
 export function useSocket() {
     const [socket, setSocket] = useState<WebSocket | null>(null);
